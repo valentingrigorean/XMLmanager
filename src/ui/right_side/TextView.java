e8b07c06c1597ba65564768767e8758cd03124d5
nbzor <valentin.grigorean1@gmail.com>
2014-05-27 20:25:54 +0300

    Refactored

--- src/ui/right_side/TextView.java
+++ src/ui/right_side/TextView.java
@@ -1,12 +1,24 @@
 package ui.right_side;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
+
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ToolTipManager;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.fife.rsta.ac.LanguageSupportFactory;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rtextarea.RTextScrollPane;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class TextView extends AbstractView {
 
@@ -14,8 +26,33 @@ public class TextView extends AbstractView {
         super();
         init();
     }
-    
-       private JTextArea createJTextArea() {
+
+    public void textFromXMLView(String s) {
+        try {
+            String text = new String();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(s));
+            org.w3c.dom.Document doc = db.parse(is);
+
+            Node n = doc.getFirstChild();
+            String nodesInfo = doc.getNodeValue();
+
+            String q = new String();
+            NodeList fi = (NodeList) n.getChildNodes();
+            System.out.println(fi.getLength());
+            for (int i = 0; i < fi.getLength(); i++) {
+                System.out.println(fi.item(i).getNodeName());
+                q += infoNode(fi.item(i));
+            }
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(TextView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private JTextArea createJTextArea() {
         RSyntaxTextArea textArea = new RSyntaxTextArea(25, 80);
         LanguageSupportFactory.get().register(textArea);
         textArea.setCaretPosition(0);
@@ -24,21 +61,37 @@ public class TextView extends AbstractView {
         textArea.setCodeFoldingEnabled(true);
         textArea.setTabsEmulated(true);
         textArea.setTabSize(3);
-        textArea.setSyntaxEditingStyle("text/xml");
+        textArea.setSyntaxEditingStyle("text/plain");
         ToolTipManager.sharedInstance().registerComponent(textArea);
         return textArea;
     }
-       
-    
+
+    private String infoNode(Node n) {
+        String text = null;
+        if (n.getNodeValue() != null && !n.getNodeValue().matches("\n *")) {
+            text += n.getNodeValue();
+            //text +='\n';
+            if (n.getNodeName().contains("#")) {
+                text += '\n';
+            }
+
+        } else {
+            NodeList p = (NodeList) n.getChildNodes();
+            for (int i = 0; i < p.getLength(); i++) {
+                infoNode(p.item(i));
+            }
+        }
+
+        return text;
+    }
+
     private void init() {
         view = createJTextArea();
         label.setText("TextView");
         scrollPane = new RTextScrollPane(view, true);
         ((JTextArea) view).setEditable(false);
-       // scrollPane = new JScrollPane(view);
+        // scrollPane = new JScrollPane(view);
         ((JScrollPane) scrollPane).setViewportView(view);
         super.add(scrollPane, BorderLayout.CENTER);
-        
-       
     }
 }
--- src/ui/right_side/ViewsPanel.java
+++ src/ui/right_side/ViewsPanel.java
@@ -25,7 +25,7 @@ import ui.utils.CPopupMenu;
 import ui.utils.ManageMenu;
 
 public class ViewsPanel extends JPanel implements Observer {
-    
+
     private TextView textView;
     private TreeView treeView;
     private XmlView xmlView;
@@ -43,15 +43,15 @@ public class ViewsPanel extends JPanel implements Observer {
         this.errorTextArea = errorTextArea;
     }
     private String text;
-    
+
     public final static int TEXT_VIEW = 2;
     public final static int XML_VIEW = 1;
     public final static int TREE_VIEW = 0;
-    
+
     public ViewsPanel() {
         init();
     }
-    
+
     @Override
     public void update(Observable o, Object arg) {
         if (arg instanceof Update) {
@@ -66,93 +66,43 @@ public class ViewsPanel extends JPanel implements Observer {
             }
         }
     }
-    
-    public String infoNode(Node n) {
-       
-        if (n.getNodeValue() != null && !n.getNodeValue().matches("\n *")) {
-            text += n.getNodeValue();
-             //text +='\n';
-            if(n.getNodeName().contains("#")) 
-             
-                text +='\n';
-            
-            
-        } else {
-            NodeList p = (NodeList) n.getChildNodes();
-            for (int i = 0; i < p.getLength(); i++) {
-                infoNode(p.item(i));
-            }
-        }
-        
-        return text;
-    }
 
-    public void textFromXMLView(String s) throws Exception {
-        text = new String();
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        
-        InputSource is = new InputSource();
-        is.setCharacterStream(new StringReader(s));
-        org.w3c.dom.Document doc = db.parse(is);
-        
-        Node n = doc.getFirstChild();
-        String nodesInfo = doc.getNodeValue();
-        
-        String q = new String();
-        NodeList fi = (NodeList) n.getChildNodes();
-        System.out.println(fi.getLength());
-        for (int i = 0; i < fi.getLength(); i++) {
-            System.out.println(fi.item(i).getNodeName());
-            q += infoNode(fi.item(i));
-        }
-    }
-    
     public void setContent(String str) {
         xmlView.setDocumentListener(false);
         ((JTextArea) xmlView.getView()).setText(str);
         xmlView.setDocumentListener(true);
-        System.out.println(str);
-        try {
-            textFromXMLView(str);
-        } catch (Exception ex) {
-            Logger.getLogger(ViewsPanel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        textView.setDocumentListener(false);
+        textView.textFromXMLView(str);
         ((JTextArea) textView.getView()).setText(text);
-        textView.setDocumentListener(true);
-
-        //System.out.println("%s", textFromXMLView(str));
         reinvalidateViews();
     }
-    
+
     public String getContent() {
         return ((JTextArea) xmlView.getView()).getText();
     }
-    
+
     public void reinvalidateViews() {
         treeView.revalidate();
     }
-    
+
     public XmlView getXmlView() {
         return xmlView;
     }
-    
+
     public TextView getXmlText() {
         return textView;
     }
-    
+
     public TreeView getXmlTree() {
         return treeView;
     }
-    
+
     public void setMainWindow(MainWindow mw) {
         this.mw = mw;
         textView.setMainWindow(mw);
         xmlView.setMainWindow(mw);
         treeView.setMainWindow(mw);
     }
-    
+
     public void showPanel(int n) {
         currItems ^= 1 << n;
         if (currItems > 0) {
@@ -164,7 +114,7 @@ public class ViewsPanel extends JPanel implements Observer {
             manageMenu.setSelected(n, true);
         }
     }
-    
+
     private void rearrange() {
         if (this.getComponent(0) != splitPane1) {
             this.removeAll();
@@ -187,8 +137,8 @@ public class ViewsPanel extends JPanel implements Observer {
                 return;
             case 4:
                 this.removeAll();
-                this.add(textView,BorderLayout.CENTER);
-                this.add(new JScrollPane(errorTextArea),BorderLayout.SOUTH);
+                this.add(textView, BorderLayout.CENTER);
+                this.add(new JScrollPane(errorTextArea), BorderLayout.SOUTH);
                 return;
             case 3:
                 splitPane1.setLeftComponent(xmlView);
@@ -196,47 +146,47 @@ public class ViewsPanel extends JPanel implements Observer {
                 return;
             case 2:
                 this.removeAll();
-                this.add(xmlView,BorderLayout.CENTER);
-                this.add(new JScrollPane(errorTextArea),BorderLayout.SOUTH);
+                this.add(xmlView, BorderLayout.CENTER);
+                this.add(new JScrollPane(errorTextArea), BorderLayout.SOUTH);
                 return;
             case 1:
                 this.removeAll();
-                this.add(treeView,BorderLayout.CENTER);
-                this.add(new JScrollPane(errorTextArea),BorderLayout.SOUTH);
+                this.add(treeView, BorderLayout.CENTER);
+                this.add(new JScrollPane(errorTextArea), BorderLayout.SOUTH);
         }
     }
-    
+
     private void init() {
         this.manageMenu = new ManageMenu();
-        
+
         textView = new TextView();
         treeView = new TreeView();
         xmlView = new XmlView();
-        
+
         splitPane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, xmlView,
                 new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
                         textView, treeView));
-        
+
         errorTextArea = new JTextArea();
         errorTextArea.setEditable(false);
-        errorTextArea.setPreferredSize(new Dimension(250,100));
-        
+        errorTextArea.setPreferredSize(new Dimension(250, 100));
+
         this.setLayout(new BorderLayout());
-        
+
         menuForViews();
-        
+
         xmlView.addObserver(this);
         textView.addObserver(this);
-        
+
         treeView.addObserver(this);
-        
+
         ((XmlOutlineTree) treeView.getView()).listenTo((RSyntaxTextArea) xmlView.getView());
         treeView.revalidate();
-        
-        this.add(splitPane1, BorderLayout.CENTER);        
+
+        this.add(splitPane1, BorderLayout.CENTER);
         this.add(new JScrollPane(errorTextArea), BorderLayout.SOUTH);
     }
-    
+
     private void hidePanels(Update upd) {
         if (upd.getView() instanceof TextView) {
             showPanel(TEXT_VIEW);
@@ -246,32 +196,25 @@ public class ViewsPanel extends JPanel implements Observer {
             showPanel(XML_VIEW);
         }
     }
-    
-    private void notifyViews(Update upd)  {
-       
-        try {
-            mw.setFileStatus(true);
-            textFromXMLView(getContent());
-            ((JTextArea) textView.getView()).setText(text);
-            //textFromXMLView(this.getContent());
-        } catch (Exception ex) {
-            Logger.getLogger(ViewsPanel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      
+
+    private void notifyViews(Update upd) {
+        mw.setFileStatus(true);       
+        textView.textFromXMLView(getContent());
+        ((JTextArea) textView.getView()).setText(text);
     }
-    
+
     private void menuForViews() {
         CPopupMenu m1 = new CPopupMenu(this);
         CPopupMenu m2 = new CPopupMenu(this);
         JPopupMenu m3 = xmlView.getPopupMenu();
         m3.addSeparator();
         m3.add(m1.getViews());
-        
+
         this.setComponentPopupMenu(m2);
         xmlView.addPopMenu(m3);
         textView.addPopMenu(m2);
         treeView.addPopMenu(m2);
-        
+
         manageMenu.setView((JMenu) m3.getComponent(12));
         manageMenu.setMenu(m2);
     }
